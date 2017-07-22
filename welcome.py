624882953b8a8fbba679aa0a52ab6b4217539a35
Azim Shaik <azim_shaik@keybank.com>
2017-07-22 16:02:09 +0000

    Revert "welcome update"
    This reverts commit aeaa2576e07e4c9f303f349fbcd9ae5b9cc557d1.

--- welcome.py
+++ welcome.py
@@ -23,7 +23,7 @@ def Welcome():
 
 @app.route('/myapp')
 def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!azim'
+    return 'Welcome again to my app running on Bluemix!'
 
 @app.route('/api/people')
 def GetPeople():
