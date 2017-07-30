1ff7d622f46db4d914c05b07af725133839c8c07
azimshaik <cheer.buddu@gmail.com>
2017-07-30 17:00:59 -0400

    Dynatable works

--- static/js/TwitterTable.js
+++ static/js/TwitterTable.js
@@ -1,27 +1,13 @@
         $(document).ready(function(){
             $.ajax({
-               url:'http://sma-key.mybluemix.net/api/watson',
+               url:'/api/watson',
                success: function(a,b,c){
                    //console.log(a);
                    $.each(a.results,function(i,v){
                         if(i==1){
-                            console.log(v);
+                            //console.log(v);
                         }
                    });
                } 
             });
-            // $.each(resp,function(i,v){
-            //     //console.log(v);
-
-            // });
-            // var resp = JSON.stringify(resp)
-            // console.log(resp)
-            // var resp = JSON.parse(resp)
-            // console.log(resp)
-            // $.each(resp, function(i,v){
-            //     $.each(v,function(index, val){
-            //         console.log(val)
-            //     });
-            // });
-            
         });
